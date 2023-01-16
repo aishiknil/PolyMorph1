75f72fe18ce8afb404fb5527c7572263c80b85da
Aishik Bera <Aishik.Bera@clinisys.com>
2023-01-16 16:57:09 +0530

    Polymirphism

---
+++ CMS.Application/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS.Application
+{
+    public class Program
+    {
+        class LoadParent
+        {
+            public void Show()
+            {
+                System.Console.WriteLine("This is Parent's Show");
+            }
+            public virtual void Test()
+            {
+                System.Console.WriteLine("This is Parents's Test");
+            }
+        }
+        class LoadChild : LoadParent
+        {
+            public void Show(int i)
+            {
+                System.Console.WriteLine("Child Class Show");
+            }
+            public override void Test()
+            {
+                System.Console.WriteLine("CHild's Test Method");
+                //base.Test();
+            }
+            static void Main(string[] args)
+            {
+                LoadChild c = new LoadChild();
+                c.Show();
+                c.Test();
+                c.Show(1);
+            }
+        }
+    }
+}
